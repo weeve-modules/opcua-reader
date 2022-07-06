d1aba9773ee3d187bfc92c44eab989258d4929cf
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-07-06 09:04:01 +0200

    no message

---
+++ image/src/config/config.js
@@ -0,0 +1,16 @@
+const env = require('../utils/env')
+
+module.exports = {
+  INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
+  INGRESS_PORT: env('INGRESS_PORT', '8080'),
+  MODULE_NAME: env('MODULE_NAME', 'OPC UA Reader'),
+  EGRESS_URL: env('EGRESS_URL', ''),
+  OPC_UA_SERVER: env('OPC_UA_SERVER', 'opc.tcp://localhost:26543'),
+  INITIAL_DELAY: env('INITIAL_DELAY', 1000),
+  MAX_RETRY: env('MAX_RETRY', 1),
+  NODE_ID: env('NODE_ID', 3),
+  VARIABLE_LIST: env('VARIABLE_LIST', 'Scalar_Simulation_Float'),
+  OPC_UA_PASSWORD: env('OPC_UA_PASSWORD', ''),
+  OPC_UA_USERNAME: env('OPC_UA_USERNAME', ''),
+  REQUIRES_AUTHENTICATION: env('REQUIRES_AUTHENTICATION', 'no'),
+}
